efad02da1ac64c10d73d927f5b41d6c19b43c902
Djamel <ddrif.djamel@gmail.com>
2016-06-23 20:12:38 +0200

    crud ok zone ok

--- src/autorisation/ObjetZone.java
+++ src/autorisation/ObjetZone.java
@@ -1,31 +1,70 @@
 package autorisation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import annuaire.Collaborateur;
 
 public class ObjetZone {
-//zone 		=> (nomZone, list[Objetporte])
+	final String lundi ="LUNDI";
+	final String mardi ="MARDI";
+	final String mercredi ="MERCREDI";
+	final String jeudi="JEUDI";
+	final String vendredi="VENDREDI";
+	final String samedi="SAMEDI";
+	final String dimanche="DIMANCHE";
+	
 	private String nomZone ;
 	
-	private ArrayList<ObjetPorte> listDeporte = new ArrayList<ObjetPorte>(); ;
+	//attention aux doublons
+	private ArrayList<ObjetPorte> listDeporte = new ArrayList<ObjetPorte>(); 
+	private HashMap<String, ObjetHoraire> listDejours  = new HashMap<String, ObjetHoraire>();
 	
-	public ObjetZone(String nomZone, String nomPorte){
+	public ObjetZone(String nomZone){
+		//pour un ponctuel
 		this.nomZone=nomZone;
-		this.listDeporte.add(new ObjetPorte(nomPorte));
-		
+		listDejours.put(lundi, new ObjetHoraire("", ""));
+		listDejours.put(mardi, new ObjetHoraire("", ""));
+		listDejours.put(mercredi, new ObjetHoraire("", ""));
+		listDejours.put(jeudi, new ObjetHoraire("", ""));
+		listDejours.put(vendredi, new ObjetHoraire("", ""));
+		listDejours.put(samedi, new ObjetHoraire("", ""));
+		listDejours.put(dimanche, new ObjetHoraire("", ""));
 	}
 	
-	public void ajouteUnePorte(String nomPorte){
-		this.listDeporte.add(new ObjetPorte(nomPorte));
+	
+	public String consulteAutorizJour(String jour){
+		String aut = "";
+		System.out.println(jour);
+			if (listDejours.get(jour)!=null){
+				aut ="heure de début : "+listDejours.get(jour).getHeureDebut()
+						+", \nheure de fin : "+listDejours.get(jour).getHeureFin();
+			}else aut = "pas d'heure de début ni de fin";
+		return aut;
 	}
-
-	public ArrayList<ObjetPorte> getListDeporte() {
-		return listDeporte;
+	
+	
+	public boolean ajouterHoraireAucollab(String jour, String heureDebut, String heureFin ) {
+		boolean reponse = false;
+			if (listDejours.get(jour)!= null){
+				ObjetHoraire horaire = new ObjetHoraire(heureDebut, heureFin);
+				listDejours.put(jour, horaire);
+				reponse =true;					
+			}else {reponse = false;}
+			
+		return reponse;
 	}
-
-	public void setListDeporte(ArrayList<ObjetPorte> listDeporte) {
-		this.listDeporte = listDeporte;
+	
+	public boolean retirerHoraireAucollab(String jour){
+		boolean reponse = false;
+		if (listDejours.get(jour)!= null){
+			listDejours.put(jour, null);
+			reponse =true;
+		}
+		return reponse;
 	}
-
+	
 	public String getNomZone() {
 		return nomZone;
 	}
@@ -33,5 +72,10 @@ public class ObjetZone {
 	public void setNomZone(String nomZone) {
 		this.nomZone = nomZone;
 	}
+	
+	public void ajouteUnePorte(ObjetPorte nomPorte){
+		this.listDeporte.add(nomPorte);
+	}
+	
 	
 }
