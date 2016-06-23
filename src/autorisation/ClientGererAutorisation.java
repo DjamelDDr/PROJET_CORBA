346d8f0d72f57bb2474083e7fe355d620d7fd5cd
Djamel <ddrif.djamel@gmail.com>
2016-06-23 21:09:58 +0200

    Debut des interaction avec autrisation

--- src/autorisation/ClientGererAutorisation.java
+++ src/autorisation/ClientGererAutorisation.java
@@ -11,25 +11,31 @@ public class ClientGererAutorisation {
 		Scanner entree_empreinte = new Scanner(System.in);
 		Scanner entree_id = new Scanner(System.in);
 		Scanner zone = new Scanner(System.in);
+		Scanner scTypC = new Scanner(System.in);
 		
 		Scanner var = new Scanner(System.in);
 		int i= 3; 
 		while(i!=0){
-			System.out.println("*********Ajout Modification d'autorisation*********");
-			System.out.println("1 : Modifier");
-			System.out.println("2 : Ajouter");
-			System.out.println("0 : Quitter");
+			System.out.println("+---------------------------------------------------+");
+			System.out.println("|*********Ajout/Modification d'autorisation*********|");
+			System.out.println("+---------------------------------------------------+");
+			System.out.println("| 1 - Ajouter?                                      |");
+			System.out.println("| 2 - Retirer?                                      |");
+			System.out.println("| 0 - Terminer                                      |");
+			System.out.println("+---------------------------------------------------+");
 			int saisie= var.nextInt();
 			switch (saisie){
-			case 1://modifier
+			case 1://ajouter
 				System.out.println("Entrer l'id du collaborateur");
 				int idSal= idCollaborateur.nextInt();
+				System.out.println("Permanent ou Ponctuel? :");
+				String typa = scTypC.nextLine();	
 				System.out.println("Saisir la zone parmi A, B, C");
 				String laZone = zone.nextLine();
 				System.out.println("0 : Quitter");
 				
 			break;  	
-			case 2://ajouter
+			case 2://retirer
 				System.out.println("Entrer l'id du collaborateur");
 				int idSalA= idCollaborateur.nextInt();
 				System.out.println("Saisir la zone parmi A, B, C");
--- src/autorisation/ImplAutorisationImpl.java
+++ src/autorisation/ImplAutorisationImpl.java
@@ -1,5 +1,6 @@
 package autorisation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,27 +10,22 @@ import org.omg.CORBA.BooleanHolder;
 
 import autorisation.AutorisationPackage.autorisationCollabInterdite;
 
-public class ImplAutorisationImpl extends AutorisationPOA{
-	
-	
-	public String[] zoneA={"P1","P2","P3"};
-	public String[] zoneB={"P4","P5","P6"};
-	public String[] zoneC={"P7","P8","P9"};
+public class ImplAutorisationImpl extends AutorisationPOA{	
 	public HashMap<Long, String[]> historisation = 
 			new HashMap<Long, String[]>();
 	
+	public HashMap<Collaborateur, ObjetZone> autoriser = new HashMap<Collaborateur, ObjetZone>();
+	
 	@Override
-	public boolean autorisationPermanent(int id, String heureDebut,
+	public boolean autorisationPermanent(Collaborateur c, String heureDebut,
 			String heureFin, String numPorte)
 			throws autorisationCollabInterdite {
 		// TODO Auto-generated method stub
-		
-		
 		return false;
 	}
 
 	@Override
-	public boolean autorisationPonctuel(int id, String jourDebut,
+	public boolean autorisationPonctuel(Collaborateur c, String jourDebut,
 			String jourFin, String heureDebut, String heureFin, String numPorte)
 			throws autorisationCollabInterdite {
 		// TODO Auto-generated method stub
@@ -51,5 +47,32 @@ public class ImplAutorisationImpl extends AutorisationPOA{
 			break;
 		}
 	}
+	
 
+	public ImplAutorisationImpl() {
+		super();		
+		zone_1.ajouteUnePorte(p11);zone_1.ajouteUnePorte(p12);zone_1.ajouteUnePorte(p13);
+		zone_2.ajouteUnePorte(p21);zone_2.ajouteUnePorte(p22);zone_2.ajouteUnePorte(p23);
+		zone_3.ajouteUnePorte(p31);zone_3.ajouteUnePorte(p32);zone_3.ajouteUnePorte(p33);
+	}
+	
+	public ObjetZone zone_1 = new ObjetZone("ZONE_1");
+	public ObjetZone zone_2 = new ObjetZone("ZONE_2");
+	public ObjetZone zone_3 = new ObjetZone("ZONE_3");
+	//list de portes pour les 3 zone
+	public ObjetPorte p11 = new ObjetPorte("P1_Z1");
+	public ObjetPorte p12 = new ObjetPorte("P2_Z1");
+	public ObjetPorte p13 = new ObjetPorte("P3_Z1");
+	public ObjetPorte p21 = new ObjetPorte("P1_Z2");
+	public ObjetPorte p22 = new ObjetPorte("P2_Z2");
+	public ObjetPorte p23 = new ObjetPorte("P3_Z2");
+	public ObjetPorte p31 = new ObjetPorte("P1_Z3");
+	public ObjetPorte p32 = new ObjetPorte("P2_Z3");
+	public ObjetPorte p33 = new ObjetPorte("P3_Z3");
+
+
+	
+	
 }
+
+
