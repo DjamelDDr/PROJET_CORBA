92ab8787a2eef2c7ffcfc4c4a3e0d9ac0a3f1a5d
Djamel <ddrif.djamel@gmail.com>
2016-06-23 21:21:26 +0200

    Suppression des anciennes méthodes distantes suite à la compil odl

--- src/gestionDesComptes/ImplGestionDesComptes.java
+++ src/gestionDesComptes/ImplGestionDesComptes.java
@@ -5,27 +5,4 @@ import gestionDesComptes.CreationDeComptePackage.collabInconnu;
 
 public class ImplGestionDesComptes extends CreationDeComptePOA{
 	
-	@Override
-	public int ajouterCollab(Collaborateur c) throws collabExisteDeja {
-		
-		return 0;
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean modifierCollab(Collaborateur c) throws collabInconnu {
-		return false;
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public boolean supprimerCollab(int id, String typeDuCollb)
-			throws collabInconnu {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
